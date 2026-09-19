35c712b3ffa5430a6583da699d579f8686debf76
Simon-666 <62134166+Simon-666@users.noreply.github.com>
2026-09-19 21:35:34 +0300

    نشر إشعار فوري جديد: مرحبا بك في متجر فوكس! 🛒🔥 تجربة 1

--- notifications.js
+++ notifications.js
@@ -4,12 +4,12 @@
  */
 
 const currentNotification = {
-    "id": 1789842866532,
+    "id": 1789842929729,
     "active": true,
     "title": "مرحبا بك في متجر فوكس! 🛒🔥 تجربة 1",
     "body": "عروض وتخفيضات خاصة متوفرة الآن، تصفح المنتجات الحصرية قبل نفاد الكمية!",
     "url": "index.html#featured",
     "icon": "logo.svg",
     "badge": "logo.svg",
-    "timestamp": 1789842866532
+    "timestamp": 1789842929729
 };
